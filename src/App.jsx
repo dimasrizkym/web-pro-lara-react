461b9cdd90f5cd3f03f42e4360f0644087c456b8
Dimas Rizky M <dimasrizkym3@gmail.com>
2024-05-11 12:54:31 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="alert alert-success" role="alert">
+        Hello React!
+      </div>
+    </>
+  );
+}
+
+export default App;
